a1d710f4db50a6dc56252bcbf3f3426afbd2a489
Stepan2021 <stepajewelry@gmail.com>
2023-07-19 15:20:49 +0300

    Loading

--- sample/src/components/enginecomponents/engine-viewer.js
+++ sample/src/components/enginecomponents/engine-viewer.js
@@ -12,27 +12,18 @@ export function EngineViewer(){
   
     const { unityProvider, addEventListener, removeEventListener, sendMessage, loadingProgression, isLoaded } =
         useUnityContext({
-            loaderUrl: "./engine/UnityEngine.loader.js",
-            dataUrl: "./engine/UnityEngine.data",
-            frameworkUrl: "./engine/UnityEngine.framework.js",
-            codeUrl: "./engine/UnityEngine.wasm",
+            loaderUrl: "./engine/pr.loader.js",
+            dataUrl: "./engine/pr.data",
+            frameworkUrl: "./engine/pr.framework.js",
+            codeUrl: "./engine/pr.wasm",
             streamingAssetsUrl: "streamingassets",
         });
-
-        
-        function load(loadingPercentage){
-          
-          var obj = document.getElementById("Loadign_elems");
-          obj.style.left = loadingPercentage + '%';
-        }
 
         const loadingPercentage = Math.round(loadingProgression * 100);
          
         const [isOnboarding, setOnboarding] = useState(true);
         
         const [isCameraAllowed, setCameraAllowed] = useState(false);
-
-        /*const poezd_width = document.getElementById("loading").getBoundingClientRect().width;*/
         
 
 
@@ -99,19 +90,17 @@ export function EngineViewer(){
           <div className="engine">
               {isOnboarding ?
                 <div className="loading-overlay" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", background: "#E21A1A", display: "flex", zIndex: 4}}>                  
-                  <p id = "App_title"> <font size="5"><strong>Мобильный помощник</strong></font><br></br> по правилам безопасного поведения <br></br>на объектах железнодорожной инфраструктуры </p>
+                  <p id = "App_title"> <font size="5"><strong>Мобильный помощник</strong></font><br></br> по правилам безопасного поведения <br></br>на объектах железнодорожной инфраструктуры <br></br> <br></br> <font size="5"><strong>Добро пожаловать!</strong></font></p>
                   <p id = "Percent"> ЗАГРУЗКА... {loadingPercentage} % </p>
                   <p id="RZD_logo"><img  src="./rzd_logo.png"></img></p>
-                  <div className="Loading_poezd" style={{left: loadingPercentage}}/* onLoad={load}*/>
+                  <div className="Loading_poezd" style={{left: loadingPercentage * 2}}/* onLoad={load}*/>
                     <img src="./poezd.png"></img> 
                   </div>
-                  <div className="Loading_Line" style={{left: loadingPercentage}}>
-                    <hr color="#ffffff" width="1024px" scale="10px"></hr>
-                  </div>
+                  <hr id="line" color="#ffffff" width={window.screen.width}></hr>
                   
                 </div> : null
               }
-              <Unity unityProvider={unityProvider} style={{position: "center", width: "100%", height: "100%", overflow: "hidden", zIndex: 3}}/>  
+              <Unity unityProvider={unityProvider} style={{position: "absoute", width: "100%", height: "100%", overflow: "hidden", zIndex: 3}}/>  
             </div>
         );        
     }
